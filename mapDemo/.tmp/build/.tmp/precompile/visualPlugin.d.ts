57ac451da2231e49974973a9c7f80edc1baa9b3e
jlanstead <55502853+jlanstead@users.noreply.github.com>
2025-08-27 20:06:46 +0000

    df

--- mapDemo/.tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var mapDemoXXXXXXXXXXXX: IVisualPlugin;
-export default mapDemoXXXXXXXXXXXX;
